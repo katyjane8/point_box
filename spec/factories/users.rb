1eb8ba7ca31b6c1449dd47f308231c02be818695
katyjane8 <katy.jane8@gmail.com>
2018-01-20 17:31:40 -0700

    users and admin factories

---
+++ spec/factories/users.rb
@@ -0,0 +1,15 @@
+FactoryBot.define do
+  factory :user do
+  sequence :username do |n|
+    "user #{n}"
+    end
+    password "password"
+    role 0
+  end
+
+  factory :admin, class: User do
+    username "admin"
+    password "password"
+    role 1
+  end
+end
